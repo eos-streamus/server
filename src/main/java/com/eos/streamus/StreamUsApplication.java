d5c837ec8dbe7137da86455ef3cabca544e43f31
AkessonHenrik <henrik.akesson@outlook.com>
2020-02-14 13:51:20 +0100

    Initial commit

---
+++ src/main/java/com/eos/streamus/StreamUsApplication.java
@@ -0,0 +1,13 @@
+package com.eos.streamus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StreamUsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StreamUsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eos/streamus/StreamUsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eos.streamus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamUsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
