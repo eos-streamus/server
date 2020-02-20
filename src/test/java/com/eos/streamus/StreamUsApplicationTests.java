520c4faedd2702af0aa9c601e0ded442829c59d3
AkessonHenrik <henrik.akesson@outlook.com>
2020-02-20 14:03:29 +0100

    Add database connection test

--- src/test/java/com/eos/streamus/StreamUsApplicationTests.java
+++ src/test/java/com/eos/streamus/StreamUsApplicationTests.java
@@ -1,13 +1,34 @@
 package com.eos.streamus;
 
+import static org.junit.jupiter.api.Assertions.fail;
+
 import org.junit.jupiter.api.Test;
+import org.postgresql.ds.PGSimpleDataSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.sql.*;
 
 @SpringBootTest
 class StreamUsApplicationTests {
+    @Value("${jdbc.url}")
+    String driver;
+    @Value("${database.user}")
+    String user;
+    @Value("${database.password}")
+    String password;
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
+
+    @Test
+    void connectToDatabase() throws SQLException {
+        PGSimpleDataSource ds = new PGSimpleDataSource();  // Empty instance.
+        ds.setURL(driver);
+        ds.setUser(user);
+        ds.setPassword(password);
+        ds.getConnection();
+    }
 
 }
