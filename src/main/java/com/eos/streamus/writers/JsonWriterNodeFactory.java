4d19a98e6ff1561bcdead12bad634c8096a017ea
AkessonHenrik <henrik.akesson@heig-vd.ch>
2020-06-07 22:15:59 +0200

    Create JsonWriterNodeFactory.java

---
+++ src/main/java/com/eos/streamus/writers/JsonWriterNodeFactory.java
@@ -0,0 +1,8 @@
+package com.eos.streamus.writers;
+
+import com.fasterxml.jackson.databind.node.JsonNodeFactory;
+
+public class JsonWriterNodeFactory extends JsonNodeFactory {
+  private static final long serialVersionUID = -5662149412033248233L;
+
+}
