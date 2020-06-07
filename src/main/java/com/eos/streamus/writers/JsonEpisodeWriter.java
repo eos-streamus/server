8b2db122d3c7319fcbe696d80c358b400aeeb8bc
AkessonHenrik <henrik.akesson@heig-vd.ch>
2020-06-07 22:17:15 +0200

    Create JsonEpisodeWriter.java

---
+++ src/main/java/com/eos/streamus/writers/JsonEpisodeWriter.java
@@ -0,0 +1,22 @@
+package com.eos.streamus.writers;
+
+import com.eos.streamus.models.Series;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+public class JsonEpisodeWriter extends JsonResourceWriter {
+  private final Series.Episode episode;
+
+  public JsonEpisodeWriter(final Series.Episode episode) {
+    super(episode);
+    this.episode = episode;
+  }
+
+  @Override
+  protected ObjectNode addSpecificJsonAttributes(final ObjectNode objectNode) {
+    return objectNode
+        .put("idSeries", episode.getSeries().getId())
+        .put("seasonNumber", episode.getSeasonNumber())
+        .put("episodeNumber", episode.getEpisodeNumber());
+  }
+
+}
