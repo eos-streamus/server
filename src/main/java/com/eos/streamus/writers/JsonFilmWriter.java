6564dd69b73eee77d44a77308a6925e062e89f48
AkessonHenrik <henrik.akesson@heig-vd.ch>
2020-06-07 22:16:28 +0200

    Create JsonFilmWriter.java

---
+++ src/main/java/com/eos/streamus/writers/JsonFilmWriter.java
@@ -0,0 +1,17 @@
+package com.eos.streamus.writers;
+
+import com.eos.streamus.models.Film;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+public class JsonFilmWriter extends JsonResourceWriter {
+
+  public JsonFilmWriter(final Film film) {
+    super(film);
+  }
+
+  @Override
+  protected ObjectNode addSpecificJsonAttributes(final ObjectNode objectNode) {
+    return objectNode;
+  }
+
+}
