5741c9d09774491f3f20891395bd2413757fead3
AkessonHenrik <henrik.akesson@heig-vd.ch>
2020-06-07 23:17:39 +0200

    Make JsonArrayWriter and JsonObjectWriter package protected classes

--- src/main/java/com/eos/streamus/writers/JsonArrayWriter.java
+++ src/main/java/com/eos/streamus/writers/JsonArrayWriter.java
@@ -3,7 +3,7 @@ package com.eos.streamus.writers;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ArrayNode;
 
-public abstract class JsonArrayWriter implements JsonWriter {
+abstract class JsonArrayWriter implements JsonWriter {
   @Override
   public final JsonNode getJson() {
     ArrayNode arrayNode = new ArrayNode(new JsonWriterNodeFactory());
--- src/main/java/com/eos/streamus/writers/JsonObjectWriter.java
+++ src/main/java/com/eos/streamus/writers/JsonObjectWriter.java
@@ -3,7 +3,7 @@ package com.eos.streamus.writers;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
-public abstract class JsonObjectWriter implements JsonWriter {
+abstract class JsonObjectWriter implements JsonWriter {
   @Override
   public final JsonNode getJson() {
     final ObjectNode objectNode = new ObjectNode(new JsonWriterNodeFactory());
