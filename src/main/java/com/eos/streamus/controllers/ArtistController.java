da251b0f9af2166307fe92648fcf9f2b864d7569
AkessonHenrik <henrik.akesson@heig-vd.ch>
2020-06-08 12:10:06 +0200

    Add get artist endpoint

--- src/main/java/com/eos/streamus/controllers/ArtistController.java
+++ src/main/java/com/eos/streamus/controllers/ArtistController.java
@@ -1,12 +1,20 @@
 package com.eos.streamus.controllers;
 
+import com.eos.streamus.exceptions.NoResultException;
 import com.eos.streamus.models.Artist;
 import com.eos.streamus.models.ArtistDAO;
+import com.eos.streamus.models.Band;
+import com.eos.streamus.models.Musician;
 import com.eos.streamus.utils.TestDatabaseConnection;
 import com.eos.streamus.writers.JsonArtistListWriter;
+import com.eos.streamus.writers.JsonBandWriter;
+import com.eos.streamus.writers.JsonMusicianWriter;
+import com.eos.streamus.writers.JsonWriter;
 import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.sql.Connection;
@@ -29,4 +37,18 @@ public class ArtistController {
     }
     return new JsonArtistListWriter(allArtists).getJson();
   }
+
+  @GetMapping("/artist/{id}")
+  public ResponseEntity<JsonNode> artist(@PathVariable int id) throws SQLException {
+    try (Connection connection = databaseConnection.getConnection()) {
+      Artist artist = ArtistDAO.findById(id, connection);
+      JsonWriter writer = artist instanceof Band ?
+          new JsonBandWriter((Band) artist) :
+          new JsonMusicianWriter((Musician) artist);
+      return ResponseEntity.ok().body(writer.getJson());
+    } catch (NoResultException e) {
+      return ResponseEntity.notFound().build();
+    }
+  }
+
 }
