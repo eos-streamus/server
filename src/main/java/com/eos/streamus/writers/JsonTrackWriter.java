bfa3aa977b359263ad3024efb5ad9d5e1b8c83c2
AkessonHenrik <henrik.akesson@heig-vd.ch>
2020-06-08 09:22:21 +0200

    Remove JsonSongWriter

--- src/main/java/com/eos/streamus/writers/JsonTrackWriter.java
+++ src/main/java/com/eos/streamus/writers/JsonTrackWriter.java
@@ -3,8 +3,9 @@ package com.eos.streamus.writers;
 import com.eos.streamus.models.SongCollection;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
-public class JsonTrackWriter extends JsonSongWriter {
+public class JsonTrackWriter extends JsonResourceWriter {
   private final SongCollection.Track track;
+
   public JsonTrackWriter(final SongCollection.Track track) {
     super(track.getValue());
     this.track = track;
@@ -12,7 +13,6 @@ public class JsonTrackWriter extends JsonSongWriter {
 
   @Override
   protected ObjectNode addSpecificJsonAttributes(final ObjectNode objectNode) {
-    super.addSpecificJsonAttributes(objectNode);
     objectNode.put("trackNumber", track.getKey());
     return objectNode;
   }
