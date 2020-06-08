13e2fb3dd9e8fe57252672b00896513e80e02e41
AkessonHenrik <henrik.akesson@heig-vd.ch>
2020-06-08 10:17:14 +0200

    Create JsonMusicianWriter.java

---
+++ src/main/java/com/eos/streamus/writers/JsonMusicianWriter.java
@@ -0,0 +1,23 @@
+package com.eos.streamus.writers;
+
+import com.eos.streamus.models.Musician;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+public class JsonMusicianWriter extends JsonArtistWriter {
+  private final Musician musician;
+
+  public JsonMusicianWriter(final Musician musician) {
+    super(musician);
+    this.musician = musician;
+  }
+
+  @Override
+  protected JsonNode addSpecificArtistJson(final ObjectNode objectNode) {
+    if (musician.getPerson() != null) {
+      new JsonPersonWriter(musician.getPerson()).getSpecificJson(objectNode);
+    }
+    return objectNode;
+  }
+
+}
