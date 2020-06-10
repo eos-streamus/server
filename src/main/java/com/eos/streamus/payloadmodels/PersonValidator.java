3e8abf83755f5645ef368798ce54ff2f9cf6c54c
AkessonHenrik <henrik.akesson@heig-vd.ch>
2020-06-10 20:21:10 +0200

    Add payloadmodels.Person, PersonValidator

---
+++ src/main/java/com/eos/streamus/payloadmodels/PersonValidator.java
@@ -0,0 +1,34 @@
+package com.eos.streamus.payloadmodels;
+
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+@Component
+public class PersonValidator implements Validator {
+  @Override
+  public boolean supports(final Class<?> aClass) {
+    return aClass.equals(Person.class);
+  }
+
+  @Override
+  public void validate(final Object o, final Errors errors) {
+    Person person = (Person) o;
+    if ((person.getFirstName() == null) ^ (person.getLastName() == null)) {
+      errors.reject("<firstName> and <lastName> must either both be defined, or not");
+    }
+    if (person.getId() == null && person.getFirstName() == null) {
+      errors.reject("Either <id> or (<firstName> and <lastName>) should be defined");
+    }
+    if (person.getDateOfBirth() != null && person.getFirstName() == null) {
+      errors.reject("<dateOfBirth> cannot be defined if <firstName> and <lastName> are not");
+    }
+    if (person.getFirstName() != null && person.getFirstName().isEmpty()) {
+      errors.reject("<firstName> cannot be empty");
+    }
+    if (person.getLastName() != null && person.getLastName().isEmpty()) {
+      errors.reject("<lastName> cannot be empty");
+    }
+  }
+
+}
