38f70c710197272608bd21c6483a96343a32af09
AkessonHenrik <henrik.akesson@heig-vd.ch>
2020-06-10 22:47:26 +0200

    Create BandMemberValidator.java

---
+++ src/main/java/com/eos/streamus/payloadmodels/BandMemberValidator.java
@@ -0,0 +1,29 @@
+package com.eos.streamus.payloadmodels;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+@Component
+public class BandMemberValidator implements Validator {
+  @Autowired
+  private MusicianValidator musicianValidator;
+
+  @Override
+  public boolean supports(final Class<?> aClass) {
+    return aClass.equals(BandMember.class);
+  }
+
+  @Override
+  public void validate(final Object o, final Errors errors) {
+    BandMember bandMember = (BandMember) o;
+    if (bandMember.getMusician() != null) {
+      musicianValidator.validate(bandMember.getMusician(), errors);
+    }
+    if (bandMember.getMusician() == null && bandMember.getMusicianId() == null) {
+      errors.reject("Invalid musician data");
+    }
+  }
+
+}
