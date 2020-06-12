13d42ba02cbbdd1fea4b2496024e960b280b53a0
AkessonHenrik <henrik.akesson@heig-vd.ch>
2020-06-13 00:41:06 +0200

    Create JsonAlbumListWriter.java

---
+++ src/main/java/com/eos/streamus/writers/JsonAlbumListWriter.java
@@ -0,0 +1,24 @@
+package com.eos.streamus.writers;
+
+import com.eos.streamus.models.Album;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+
+import java.util.List;
+
+public class JsonAlbumListWriter extends JsonArrayWriter {
+  private final List<Album> albums;
+
+  public JsonAlbumListWriter(final List<Album> albums) {
+    this.albums = albums;
+  }
+
+  @Override
+  protected JsonNode getSpecificArrayJson(final ArrayNode arrayNode) {
+    for (Album album : albums) {
+      arrayNode.add(new JsonAlbumWriter(album).getJson());
+    }
+    return arrayNode;
+  }
+
+}
