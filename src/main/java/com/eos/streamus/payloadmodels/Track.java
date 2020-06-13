c6e6364a61793b31d59edae7be75edaf0e7bb008
AkessonHenrik <henrik.akesson@heig-vd.ch>
2020-06-13 18:52:07 +0200

    Create Track payload model

---
+++ src/main/java/com/eos/streamus/payloadmodels/Track.java
@@ -0,0 +1,15 @@
+package com.eos.streamus.payloadmodels;
+
+public class Track {
+  private int trackNumber;
+  private int songId;
+
+  public int getTrackNumber() {
+    return trackNumber;
+  }
+
+  public int getSongId() {
+    return songId;
+  }
+
+}
