7fd9c578284108321b0d434e8c1b722bc642c597
AkessonHenrik <henrik.akesson@heig-vd.ch>
2020-06-14 00:44:05 +0200

    Move Person test to own class

---
+++ src/test/java/com/eos/streamus/models/PersonTests.java
@@ -0,0 +1,42 @@
+package com.eos.streamus.models;
+
+import com.eos.streamus.DatabaseTests;
+import com.eos.streamus.exceptions.NoResultException;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@SpringBootTest
+public class PersonTests extends DatabaseTests {
+  @Test
+  void testPersonCRUD() throws SQLException, NoResultException {
+    try (Connection connection = databaseConnection.getConnection()) {
+      // Create
+      Person person = new Person("John", "Doe", randomDate());
+      person.save(connection);
+
+      // Read
+      Person retrievedPerson = Person.findById(person.getId(), connection);
+      assertEquals(person, retrievedPerson);
+
+      // Update
+      person.setFirstName("Jane");
+      person.setLastName("Donut");
+      person.setDateOfBirth(randomDate());
+      person.save(connection);
+
+      retrievedPerson = Person.findById(person.getId(), connection);
+      assertEquals(person, retrievedPerson);
+
+      // Delete
+      person.delete(connection);
+      assertThrows(NoResultException.class, () -> Person.findById(person.getId(), connection));
+    }
+  }
+
+}
--- src/test/java/com/eos/streamus/models/StreamUsApplicationTests.java
+++ src/test/java/com/eos/streamus/models/StreamUsApplicationTests.java
@@ -29,32 +29,6 @@ class StreamUsApplicationTests {
   private final Random random = new Random();
 
   @Test
-  void testPersonCRUD() throws SQLException, NoResultException {
-    try (Connection connection = databaseConnection.getConnection()) {
-      // Create
-      Person person = new Person("John", "Doe", randomDate());
-      person.save(connection);
-
-      // Read
-      Person retrievedPerson = Person.findById(person.getId(), connection);
-      assertEquals(person, retrievedPerson);
-
-      // Update
-      person.setFirstName("Jane");
-      person.setLastName("Donut");
-      person.setDateOfBirth(randomDate());
-      person.save(connection);
-
-      retrievedPerson = Person.findById(person.getId(), connection);
-      assertEquals(person, retrievedPerson);
-
-      // Delete
-      person.delete(connection);
-      assertThrows(NoResultException.class, () -> Person.findById(person.getId(), connection));
-    }
-  }
-
-  @Test
   void testUserCRUD() throws SQLException, NoResultException {
     try (Connection connection = databaseConnection.getConnection()) {
       // Create
