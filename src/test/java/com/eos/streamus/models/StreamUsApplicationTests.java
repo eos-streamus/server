a9cfdcbb5bcac3063608607a9012941aab25e366
AkessonHenrik <henrik.akesson@heig-vd.ch>
2020-06-14 00:41:36 +0200

    Remove connectToDatabase and testSongCRUD from StreamUsApplicationTests

--- src/test/java/com/eos/streamus/models/StreamUsApplicationTests.java
+++ src/test/java/com/eos/streamus/models/StreamUsApplicationTests.java
@@ -29,41 +29,6 @@ class StreamUsApplicationTests {
   private final Random random = new Random();
 
   @Test
-  void connectToDatabase() {
-    assertDoesNotThrow(() -> {
-      Connection connection = databaseConnection.getConnection();
-      connection.close();
-    });
-  }
-
-  @Test
-  void testSongCRUD() throws SQLException, NoResultException {
-    try (Connection connection = databaseConnection.getConnection()) {
-      // Create
-      Song song = new Song(String.format("test%d.mp3", new Date().getTime()), randomString(), 100);
-      song.save(connection);
-
-      // Read
-      Song retrievedSong = Song.findById(song.getId(), connection);
-      assertEquals(song, retrievedSong);
-
-      // Update
-      song.setName("Changed song name");
-      song.setPath(String.format("test%d.mp3", new Date().getTime()));
-      song.setDuration(101);
-      song.save(connection);
-      song.save(connection);
-
-      retrievedSong = Song.findById(song.getId(), connection);
-      assertEquals(song, retrievedSong);
-
-      // Delete
-      song.delete(connection);
-      assertThrows(NoResultException.class, () -> Song.findById(song.getId(), connection));
-    }
-  }
-
-  @Test
   void testFilmCRUD() throws SQLException, NoResultException {
     try (Connection connection = databaseConnection.getConnection()) {
       // Create
