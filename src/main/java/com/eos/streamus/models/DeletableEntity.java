748cfb1d9fe5725bf7b23cbc5ae8a0bcf87f850d
AkessonHenrik <henrik.akesson@heig-vd.ch>
2020-06-13 21:42:23 +0200

    Update DeletableEntity.java

--- src/main/java/com/eos/streamus/models/DeletableEntity.java
+++ src/main/java/com/eos/streamus/models/DeletableEntity.java
@@ -6,9 +6,16 @@ import java.sql.SQLException;
 
 public interface DeletableEntity extends Deletable, Entity {
   default void delete(Connection connection) throws SQLException {
-    try (PreparedStatement preparedStatement = connection.prepareStatement(String.format("delete from %s where %s = ?;", tableName(), primaryKeyName()))) {
+    try (PreparedStatement preparedStatement = connection.prepareStatement(
+        String.format(
+            "delete from %s where %s = ?;",
+            tableName(),
+            primaryKeyName()
+        )
+    )) {
       preparedStatement.setInt(1, getId());
       preparedStatement.execute();
     }
   }
+
 }
