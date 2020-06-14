2ad2e9d9fab27d393f32704d081a7176dd2601e0
AkessonHenrik <henrik.akesson@heig-vd.ch>
2020-06-14 19:09:11 +0200

    Update StreamUsApplicationTests.java

--- src/test/java/com/eos/streamus/models/StreamUsApplicationTests.java
+++ src/test/java/com/eos/streamus/models/StreamUsApplicationTests.java
@@ -1,48 +1,7 @@
 package com.eos.streamus.models;
 
-import com.eos.streamus.utils.TestDatabaseConnection;
-import com.eos.streamus.exceptions.NoResultException;
-import org.junit.jupiter.api.Test;
-import org.postgresql.util.PSQLException;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.sql.Connection;
-import java.sql.SQLException;
-import java.sql.SQLTransactionRollbackException;
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.Random;
-
-import static org.junit.jupiter.api.Assertions.*;
-import static java.sql.Date.valueOf;
 
 @SpringBootTest
 class StreamUsApplicationTests {
-  @Autowired
-  protected TestDatabaseConnection databaseConnection = null;
-
-  private final Random random = new Random();
-
-  private String randomString() {
-    return "randomString" + random.nextDouble();
-  }
-
-  private java.sql.Date randomDate() {
-    int year = random.nextInt() % 70 + 1940;
-    int month = (random.nextInt() & Integer.MAX_VALUE) % 12 + 1;
-    return valueOf(String.format("%d-%s-01", year, (month < 10 ? "0" + month : month)));
-  }
-
-  private Person randomPerson() {
-    return new Person(randomString(), randomString(), randomDate());
-  }
-
-  private User randomUser() {
-    return new User(randomString(), randomString(), randomDate(), randomString() + "@" + randomString(), randomString());
-  }
 }
