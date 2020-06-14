137920b38ce2b886050b212f3bffd5dca8fe230b
AkessonHenrik <henrik.akesson@heig-vd.ch>
2020-06-15 01:57:00 +0200

    Add annotation to SongPlaylistController

--- src/main/java/com/eos/streamus/controllers/SongPlaylistController.java
+++ src/main/java/com/eos/streamus/controllers/SongPlaylistController.java
@@ -9,10 +9,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.sql.Connection;
 import java.sql.SQLException;
 
+@RestController
 public class SongPlaylistController implements CommonResponses {
 
   private final DatabaseConnection databaseConnection;
