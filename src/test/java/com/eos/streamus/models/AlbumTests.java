d391e2860f720c49eb1c4c607c5d4b35c3f04e6f
AkessonHenrik <henrik.akesson@heig-vd.ch>
2020-06-14 10:17:49 +0200

    Move Album tests to own class

---
+++ src/test/java/com/eos/streamus/models/AlbumTests.java
@@ -0,0 +1,74 @@
+package com.eos.streamus.models;
+
+import com.eos.streamus.DatabaseTests;
+import com.eos.streamus.exceptions.NoResultException;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
+public class AlbumTests extends DatabaseTests {
+
+  @Test
+  void testAlbum() throws SQLException, NoResultException {
+    try (Connection connection = databaseConnection.getConnection()) {
+      Album album = new Album(randomString(), randomDate());
+
+      Song[] songs = new Song[10];
+      for (int i = 0; i < 10; i++) {
+        songs[i] = new Song(randomString(), randomString(), 100);
+        songs[i].save(connection);
+        album.addSong(songs[i]);
+      }
+      album.save(connection);
+      Album fetchedAlbum = Album.findById(album.getId(), connection);
+      assertEquals(album.getContent().size(), fetchedAlbum.getContent().size());
+      assertEquals(album, fetchedAlbum);
+
+      album.delete(connection);
+      for (Song song : songs) {
+        song.delete(connection);
+      }
+    }
+  }
+
+  @Test
+  void testAlbumWithArtists() throws SQLException, NoResultException {
+    try (Connection connection = databaseConnection.getConnection()) {
+      Band band = new Band("test band");
+      band.save(connection);
+
+      Musician musician = new Musician(randomPerson());
+      musician.save(connection);
+
+      Album album = new Album(randomString(), randomDate());
+
+      album.addArtist(band);
+      album.addArtist(musician);
+
+      Song[] songs = new Song[10];
+      for (int i = 0; i < 10; i++) {
+        songs[i] = new Song(randomString(), randomString(), 100);
+        songs[i].save(connection);
+        album.addSong(songs[i]);
+      }
+      album.save(connection);
+      Album fetchedAlbum = Album.findById(album.getId(), connection);
+      assertEquals(album.getContent().size(), fetchedAlbum.getContent().size());
+      assertEquals(album, fetchedAlbum);
+
+      album.delete(connection);
+      for (Song song : songs) {
+        song.delete(connection);
+      }
+      musician.getPerson().delete(connection);
+      musician.delete(connection);
+      band.delete(connection);
+    }
+  }
+
+}
--- src/test/java/com/eos/streamus/models/StreamUsApplicationTests.java
+++ src/test/java/com/eos/streamus/models/StreamUsApplicationTests.java
@@ -28,64 +28,6 @@ class StreamUsApplicationTests {
 
   private final Random random = new Random();
 
-  @Test
-  void testAlbum() throws SQLException, NoResultException {
-    try (Connection connection = databaseConnection.getConnection()) {
-      Album album = new Album(randomString(), randomDate());
-
-      Song[] songs = new Song[10];
-      for (int i = 0; i < 10; i++) {
-        songs[i] = new Song(randomString(), randomString(), 100);
-        songs[i].save(connection);
-        album.addSong(songs[i]);
-      }
-      album.save(connection);
-      Album fetchedAlbum = Album.findById(album.getId(), connection);
-      assertEquals(album.getContent().size(), fetchedAlbum.getContent().size());
-      assertEquals(album, fetchedAlbum);
-
-      album.delete(connection);
-      for (Song song : songs) {
-        song.delete(connection);
-      }
-    }
-  }
-
-  @Test
-  void testAlbumWithArtists() throws SQLException, NoResultException {
-    try (Connection connection = databaseConnection.getConnection()) {
-      Band band = new Band("test band");
-      band.save(connection);
-
-      Musician musician = new Musician(randomPerson());
-      musician.save(connection);
-
-      Album album = new Album(randomString(), randomDate());
-
-      album.addArtist(band);
-      album.addArtist(musician);
-
-      Song[] songs = new Song[10];
-      for (int i = 0; i < 10; i++) {
-        songs[i] = new Song(randomString(), randomString(), 100);
-        songs[i].save(connection);
-        album.addSong(songs[i]);
-      }
-      album.save(connection);
-      Album fetchedAlbum = Album.findById(album.getId(), connection);
-      assertEquals(album.getContent().size(), fetchedAlbum.getContent().size());
-      assertEquals(album, fetchedAlbum);
-
-      album.delete(connection);
-      for (Song song : songs) {
-        song.delete(connection);
-      }
-      musician.getPerson().delete(connection);
-      musician.delete(connection);
-      band.delete(connection);
-    }
-  }
-
   private String randomString() {
     return "randomString" + random.nextDouble();
   }
