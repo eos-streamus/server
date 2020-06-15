0870916d7d57654a01f4e3843566379affa51060
AkessonHenrik <henrik.akesson@heig-vd.ch>
2020-06-15 13:09:01 +0200

    Generalize common validations ebtween AlbumValidator and SongPlaylistValidator in SongCollectionValidator

--- src/main/java/com/eos/streamus/payloadmodels/validators/AlbumValidator.java
+++ src/main/java/com/eos/streamus/payloadmodels/validators/AlbumValidator.java
@@ -2,21 +2,17 @@ package com.eos.streamus.payloadmodels.validators;
 
 import com.eos.streamus.exceptions.NoResultException;
 import com.eos.streamus.models.ArtistDAO;
-import com.eos.streamus.models.Song;
 import com.eos.streamus.payloadmodels.Album;
-import com.eos.streamus.payloadmodels.Track;
 import com.eos.streamus.utils.DatabaseConnection;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.validation.Errors;
-import org.springframework.validation.Validator;
 
 import java.sql.Connection;
 import java.sql.SQLException;
-import java.util.Comparator;
 
 @Component
-public class AlbumValidator implements Validator {
+public class AlbumValidator extends SongCollectionValidator {
 
   @Autowired
   private DatabaseConnection databaseConnection;
@@ -27,33 +23,18 @@ public class AlbumValidator implements Validator {
   }
 
   @Override
-  public void validate(final Object o, final Errors errors) {
+  protected void validateSubclassProperties(final Object o, final Errors errors) {
     Album album = (Album) o;
-
-    if (album.getName().isBlank()) {
-      errors.reject("Invalid album name");
-    }
-
     try (Connection connection = databaseConnection.getConnection()) {
       for (Integer artistId : album.getArtistIds()) {
         ArtistDAO.findById(artistId, connection);
-      }
-      album.getTracks().sort(Comparator.comparingInt(Track::getTrackNumber));
-      for (int i = 0; i < album.getTracks().size(); i++) {
-        if (album.getTracks().get(i).getTrackNumber() < 1) {
-          errors.reject("Invalid track number, must be >= 1");
-        }
-        Song.findById(album.getTracks().get(i).getSongId(), connection);
-        if ((i == 0 && album.getTracks().get(i).getTrackNumber() != 1) ||
-            (i > 0 && album.getTracks().get(i - 1).getTrackNumber() != album.getTracks().get(i).getTrackNumber() - 1)) {
-          errors.reject("Invalid track numbers");
-        }
       }
     } catch (SQLException sqlException) {
       errors.reject("SQL Error");
     } catch (NoResultException noResultException) {
       errors.reject("Invalid artist or song ids");
     }
+
   }
 
 }
