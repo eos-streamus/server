017d14b3d7ce7e52278cf519ce51c89684d77fae
AkessonHenrik <henrik.akesson@heig-vd.ch>
2020-06-15 03:46:04 +0200

    Create SongPlaylistValidator.java

---
+++ src/main/java/com/eos/streamus/payloadmodels/validators/SongPlaylistValidator.java
@@ -0,0 +1,63 @@
+package com.eos.streamus.payloadmodels.validators;
+
+import com.eos.streamus.exceptions.NoResultException;
+import com.eos.streamus.models.Song;
+import com.eos.streamus.models.User;
+import com.eos.streamus.payloadmodels.SongPlaylist;
+import com.eos.streamus.payloadmodels.Track;
+import com.eos.streamus.utils.DatabaseConnection;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.Validator;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.Comparator;
+
+@Component
+public class SongPlaylistValidator implements Validator {
+
+  @Autowired
+  private DatabaseConnection databaseConnection;
+
+  @Override
+  public boolean supports(final Class<?> aClass) {
+    return aClass.equals(SongPlaylist.class);
+  }
+
+  @Override
+  public void validate(final Object o, final Errors errors) {
+    SongPlaylist songPlaylist = (SongPlaylist) o;
+
+    if (songPlaylist.getName().isBlank()) {
+      errors.reject("Invalid playlist name");
+    }
+
+    try (Connection connection = databaseConnection.getConnection()) {
+      User.findById(songPlaylist.getUserId(), connection);
+      songPlaylist.getTracks().sort(Comparator.comparingInt(Track::getTrackNumber));
+      for (int i = 0; i < songPlaylist.getTracks().size(); i++) {
+        if (songPlaylist.getTracks().get(i).getTrackNumber() < 1) {
+          errors.reject("Invalid track number, must be >= 1");
+        }
+        Song.findById(songPlaylist.getTracks().get(i).getSongId(), connection);
+        if (
+            (i == 0 && songPlaylist.getTracks().get(i).getTrackNumber() != 1) ||
+            (
+                i > 0 &&
+                songPlaylist.getTracks().get(i - 1).getTrackNumber() !=
+                songPlaylist.getTracks().get(i).getTrackNumber() - 1
+            )
+        ) {
+          errors.reject("Invalid track numbers");
+        }
+      }
+    } catch (SQLException sqlException) {
+      errors.reject("SQL Error");
+    } catch (NoResultException noResultException) {
+      errors.reject("Invalid artist or song ids");
+    }
+  }
+
+}
