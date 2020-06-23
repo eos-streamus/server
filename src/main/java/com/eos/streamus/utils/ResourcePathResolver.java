940b41453afb8a3d5629078eddcc6ba3917611bd
AkessonHenrik <henrik.akesson@heig-vd.ch>
2020-06-23 21:40:53 +0200

    Make ResourcePathResolver implement IResourcePathResolver

--- src/main/java/com/eos/streamus/utils/ResourcePathResolver.java
+++ src/main/java/com/eos/streamus/utils/ResourcePathResolver.java
@@ -8,14 +8,16 @@ import java.io.File;
 
 @Service
 @Scope(value = "singleton")
-public class ResourcePathResolver {
+public class ResourcePathResolver implements IResourcePathResolver {
   @Value("${resourcePath}")
   private String resourcePath;
 
+  @Override
   public String getVideoDir() {
     return String.format("%s%s%s%s", resourcePath, File.separator, "video", File.separator);
   }
 
+  @Override
   public String getAudioDir() {
     return String.format("%s%s%s%s", resourcePath, File.separator, "audio", File.separator);
   }
