06491c633bde83f7261348085b4da9919e0e2905
AkessonHenrik <henrik.akesson@heig-vd.ch>
2020-06-23 21:40:40 +0200

    Create TestResourcePathResolver.java

---
+++ src/test/java/com/eos/streamus/utils/TestResourcePathResolver.java
@@ -0,0 +1,24 @@
+package com.eos.streamus.utils;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Service;
+
+import java.io.File;
+
+@Service
+@Scope(value = "singleton")
+public class TestResourcePathResolver implements IResourcePathResolver {
+  @Value("${testResourcePath}")
+  private String resourcePath;
+
+  @Override
+  public String getVideoDir() {
+    return String.format("%s%s%s%s", resourcePath, File.separator, "video", File.separator);
+  }
+
+  @Override
+  public String getAudioDir() {
+    return String.format("%s%s%s%s", resourcePath, File.separator, "audio", File.separator);
+  }
+}
