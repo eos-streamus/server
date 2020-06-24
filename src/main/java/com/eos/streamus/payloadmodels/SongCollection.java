10385db3b5884d18e5bf7ee661418f9902251942
AkessonHenrik <henrik.akesson@heig-vd.ch>
2020-06-24 20:27:30 +0200

    Add min name length constraint to payload SongCollection

--- src/main/java/com/eos/streamus/payloadmodels/SongCollection.java
+++ src/main/java/com/eos/streamus/payloadmodels/SongCollection.java
@@ -1,10 +1,12 @@
 package com.eos.streamus.payloadmodels;
 
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 import java.util.List;
 
 public abstract class SongCollection {
   @NotNull
+  @Size(min = 1)
   private String name;
   @NotNull
   private List<Track> tracks;
