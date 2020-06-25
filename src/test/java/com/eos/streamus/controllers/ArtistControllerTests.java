e94aed730c583124660072754139948248474975
AkessonHenrik <henrik.akesson@heig-vd.ch>
2020-06-25 05:06:23 +0200

    Add gettingArtistsShouldReturnOkWithArray

--- src/test/java/com/eos/streamus/controllers/ArtistControllerTests.java
+++ src/test/java/com/eos/streamus/controllers/ArtistControllerTests.java
@@ -1,5 +1,31 @@
 package com.eos.streamus.controllers;
 
+import com.fasterxml.jackson.core.JsonFactory;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockHttpServletResponse;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 public class ArtistControllerTests extends ControllerTests {
-  
+
+
+  @Test
+  void gettingArtistsShouldReturnOkWithArray() throws Exception {
+    RequestBuilder builder = MockMvcRequestBuilders.get("/artists")
+                                                   .contentType(MediaType.APPLICATION_JSON);
+    MockHttpServletResponse response =
+        mockMvc
+            .perform(builder)
+            .andExpect(status().is(200))
+            .andReturn()
+            .getResponse();
+    JsonNode json = new ObjectMapper(new JsonFactory()).readTree(response.getContentAsString());
+    assertNotNull(json.iterator());
+  }
 }
