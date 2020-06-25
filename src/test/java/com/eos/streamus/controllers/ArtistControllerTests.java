cb12cfc0d1ab07669acb55b252c25d9116d14518
AkessonHenrik <henrik.akesson@heig-vd.ch>
2020-06-25 05:12:00 +0200

    Add creatingAMusicianWithAPersonAndNameShouldReturnOkWithCorrectJson

--- src/test/java/com/eos/streamus/controllers/ArtistControllerTests.java
+++ src/test/java/com/eos/streamus/controllers/ArtistControllerTests.java
@@ -370,4 +370,30 @@ public class ArtistControllerTests extends ControllerTests {
       musician.delete(connection);
     }
   }
+
+  @Test
+  void creatingAMusicianWithAPersonAndNameShouldReturnOkWithCorrectJson() throws Exception {
+    ObjectNode objectNode = new ObjectNode(new TestJsonFactory());
+    objectNode.put("name", "test name");
+    ObjectNode personObjectNode = objectNode.putObject("person");
+    personObjectNode.put("firstName", "John");
+    personObjectNode.put("lastName", "Doe");
+    personObjectNode.put("dateOfBirth", date("2000-01-01").getTime());
+
+    RequestBuilder builder = MockMvcRequestBuilders.post("/musician")
+                                                   .contentType(MediaType.APPLICATION_JSON)
+                                                   .content(objectNode.toPrettyString());
+
+    MockHttpServletResponse response = mockMvc.perform(builder)
+                                              .andExpect(status().is(200))
+                                              .andReturn()
+                                              .getResponse();
+    JsonNode jsonNode = new ObjectMapper(new JsonFactory()).readTree(response.getContentAsString());
+    assertTrue(jsonNode.has("id"));
+    try (Connection connection = databaseConnection.getConnection()) {
+      Musician musician = Musician.findById(jsonNode.get("id").asInt(), connection);
+      assertEquals(new JsonMusicianWriter(musician).getJson(), jsonNode);
+      musician.delete(connection);
+    }
+  }
 }
