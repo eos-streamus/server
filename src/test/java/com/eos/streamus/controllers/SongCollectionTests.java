16276f2d96bcb23273827a9f1e5b9ba83c682cce
AkessonHenrik <henrik.akesson@heig-vd.ch>
2020-06-27 20:19:10 +0200

    Add movingATrackInASongCollectionShouldReturnOkWithCorrectJson

--- src/test/java/com/eos/streamus/controllers/SongCollectionTests.java
+++ src/test/java/com/eos/streamus/controllers/SongCollectionTests.java
@@ -1,6 +1,8 @@
 package com.eos.streamus.controllers;
 
 import com.eos.streamus.models.Album;
+import com.eos.streamus.models.Song;
+import com.eos.streamus.models.SongCollection;
 import com.eos.streamus.models.SongPlaylist;
 import com.eos.streamus.models.User;
 import com.eos.streamus.writers.JsonAlbumWriter;
@@ -8,8 +10,10 @@ import com.eos.streamus.writers.JsonSongPlaylistWriter;
 import com.fasterxml.jackson.core.JsonFactory;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.RequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -17,6 +21,9 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -94,4 +101,38 @@ public class SongCollectionTests extends ControllerTests {
              .andReturn();
     }
   }
+
+  @Test
+  void movingATrackInASongCollectionShouldReturnOkWithCorrectJson() throws Exception {
+    try (Connection connection = databaseConnector.getConnection()) {
+      Album album = new Album("Test album", date("2000-01-01"));
+      album.save(connection);
+      List<SongCollection.Track> tracks = new ArrayList<>();
+      for (int i = 0; i < 10; i++) {
+        Song song = new Song(UUID.randomUUID().toString(), UUID.randomUUID().toString(), 100);
+        song.save(connection);
+        tracks.add(album.addSong(song));
+      }
+      album.save(connection);
+
+      SongCollection.Track trackToMove = tracks.get(2);
+      int newTrackNumber = 4;
+      ObjectNode trackData = new ObjectNode(new TestJsonFactory());
+      trackData.put("songId", trackToMove.getSong().getId());
+      trackData.put("trackNumber", newTrackNumber);
+      RequestBuilder builder = MockMvcRequestBuilders.put(String.format("/album/%d", album.getId()))
+                                                     .contentType(MediaType.APPLICATION_JSON)
+                                                     .content(trackData.toPrettyString());
+
+      MockHttpServletResponse response = mockMvc.perform(builder).andExpect(status().is(200)).andReturn().getResponse();
+      JsonNode json = new ObjectMapper(new JsonFactory()).readTree(response.getContentAsString());
+
+      for (JsonNode trackNode : json.get("tracks")) {
+        if (trackNode.get("id").asInt() == trackToMove.getSong().getId()) {
+          assertEquals(trackNode.get("trackNumber").asInt(), newTrackNumber);
+        }
+      }
+    }
+  }
+
 }
