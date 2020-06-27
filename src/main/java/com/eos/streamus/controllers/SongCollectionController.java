a7ea2fdd8fa5e114b33e30db0b3cb705ba98402b
AkessonHenrik <henrik.akesson@heig-vd.ch>
2020-06-27 19:20:22 +0200

    Remove unused import

--- src/main/java/com/eos/streamus/controllers/SongCollectionController.java
+++ src/main/java/com/eos/streamus/controllers/SongCollectionController.java
@@ -13,7 +13,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.sql.Connection;
 import java.sql.SQLException;
