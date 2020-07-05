618b91bdab7991c490e17d6aae1f1ce65e785851
AkessonHenrik <henrik.akesson@heig-vd.ch>
2020-07-05 20:42:17 +0200

    Create UserData and UserValidator

---
+++ src/main/java/com/eos/streamus/payloadmodels/UserData.java
@@ -0,0 +1,37 @@
+package com.eos.streamus.payloadmodels;
+
+import javax.validation.constraints.NotNull;
+
+public class UserData extends Person {
+  @NotNull
+  private String email;
+  @NotNull
+  private String username;
+  @NotNull
+  private String password;
+
+  public String getEmail() {
+    return email;
+  }
+
+  public void setEmail(final String email) {
+    this.email = email;
+  }
+
+  public String getUsername() {
+    return username;
+  }
+
+  public void setUsername(final String username) {
+    this.username = username;
+  }
+
+  public String getPassword() {
+    return password;
+  }
+
+  public void setPassword(final String password) {
+    this.password = password;
+  }
+
+}
---
+++ src/main/java/com/eos/streamus/payloadmodels/validators/UserValidator.java
@@ -0,0 +1,48 @@
+package com.eos.streamus.payloadmodels.validators;
+
+import com.eos.streamus.payloadmodels.UserData;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+
+import static com.eos.streamus.utils.Constants.EMAIL_REGEX;
+
+@Component
+public class UserValidator extends PersonValidator {
+  @Value("${minPasswordLength}")
+  private int minPasswordLength;
+  @Value("${minUsernameLength}")
+  private int minUsernameLength;
+
+  @Override
+  public boolean supports(final Class<?> aClass) {
+    return aClass.equals(UserData.class);
+  }
+
+  @Override
+  public void validate(final Object o, final Errors errors) {
+    super.validate(o, errors);
+    UserData userData = (UserData) o;
+
+    if (userData.getEmail().isBlank()) {
+      errors.reject("Invalid email");
+    }
+    if (!userData.getEmail().matches(EMAIL_REGEX)) {
+      errors.reject("Invalid email");
+    }
+
+    if (userData.getUsername().isBlank()) {
+      errors.reject("Username cannot be empty");
+    }
+    if (userData.getUsername().trim().length() < minUsernameLength) {
+      errors.reject("Username not long enough");
+    }
+
+    if (userData.getPassword().isBlank()) {
+      errors.reject("Password cannot be empty");
+    }
+    if (userData.getPassword().trim().length() < minPasswordLength) {
+      errors.reject("Password not long enough");
+    }
+  }
+}
