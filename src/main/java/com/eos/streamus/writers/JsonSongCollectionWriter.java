1e1b5fb12dad5a3c7c42317ec9179fa3d00ee77b
AkessonHenrik <henrik.akesson@heig-vd.ch>
2020-07-11 23:50:17 +0200

    Checkstyle: Fix JsonSongCollectionWriter

--- src/main/java/com/eos/streamus/writers/JsonSongCollectionWriter.java
+++ src/main/java/com/eos/streamus/writers/JsonSongCollectionWriter.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.databind.node.ArrayNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
 public class JsonSongCollectionWriter extends JsonCollectionWriter {
+  /** {@link com.eos.streamus.models.SongCollection} to write as Json. */
   private final SongCollection songCollection;
 
   protected JsonSongCollectionWriter(final SongCollection songCollection) {
@@ -13,6 +14,11 @@ public class JsonSongCollectionWriter extends JsonCollectionWriter {
     this.songCollection = songCollection;
   }
 
+  /**
+   * Write {@link SongCollection.Track}s to json.
+   * @param objectNode ObjectNode to write tracks to.
+   * @return Json node.
+   */
   @Override
   protected JsonNode getSpecificCollectionJson(final ObjectNode objectNode) {
     ArrayNode tracks = objectNode.putArray("tracks");
