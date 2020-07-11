2f9d756c56af69481ae2e57dcba2a06c856fd71e
AkessonHenrik <henrik.akesson@heig-vd.ch>
2020-07-11 23:16:56 +0200

    Checkstyle: Fix Video

--- src/main/java/com/eos/streamus/models/Video.java
+++ src/main/java/com/eos/streamus/models/Video.java
@@ -4,26 +4,31 @@ import java.sql.Timestamp;
 
 public abstract class Video extends Resource {
   //#region Static attributes
+  /** Table name in database. */
   private static final String TABLE_NAME = "Video";
+  /** Primary key column name. */
   private static final String PRIMARY_KEY_NAME = "idResource";
   //#endregion Static attributes
 
   //#region Constructors
-  protected Video(Integer id, String path, String name, Timestamp createdAt, Integer duration) {
+  protected Video(final Integer id, final String path, final String name,
+                  final Timestamp createdAt, final Integer duration) {
     super(id, path, name, createdAt, duration);
   }
 
-  protected Video(String path, String name, Integer duration) {
+  protected Video(final String path, final String name, final Integer duration) {
     super(path, name, duration);
   }
-  //#endregion Constructors
+  //#endregion Constructors_
 
   //#region Getters and Setters
+  /** @return Table name. */
   @Override
   public String tableName() {
     return TABLE_NAME;
   }
 
+  /** @return Primary key name. */
   @Override
   public String primaryKeyName() {
     return PRIMARY_KEY_NAME;
