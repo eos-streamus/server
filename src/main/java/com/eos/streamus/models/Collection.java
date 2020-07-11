84e7a12bd7b79a2dcd6d1211ec4e95a9e33ee51c
AkessonHenrik <henrik.akesson@heig-vd.ch>
2020-07-11 19:35:47 +0200

    Checkstyle: Fix Collection

--- src/main/java/com/eos/streamus/models/Collection.java
+++ src/main/java/com/eos/streamus/models/Collection.java
@@ -8,74 +8,86 @@ import java.util.List;
 
 public abstract class Collection implements SavableDeletableEntity {
   //#region Static attributes
+  /** Table name in database. */
   public static final String TABLE_NAME = "Collection";
+  /** id column name. */
   protected static final String PRIMARY_KEY_NAME = "id";
+  /** name column name. */
   protected static final String NAME_COLUMN = "name";
+  /** createdAt column name. */
   protected static final String CREATED_AT_COLUMN = "createdAt";
+  /** updatedAt column name. */
   protected static final String UPDATED_AT_COLUMN = "updatedAt";
   //#endregion Static attributes
 
   //#region Instance attributes
+  /** Id of instance. */
   private Integer id;
+  /** Name of Collection. */
   private String name;
+  /** Creation timestamp of collection. */
   private Timestamp createdAt;
+  /** Update timestamp of collection. */
   private Timestamp updatedAt;
   //#endregion Instance attributes
 
   //#region Constructors
-  protected Collection(Integer id, String name, Timestamp createdAt, Timestamp updatedAt) {
+  protected Collection(final Integer id, final String name, final Timestamp createdAt, final Timestamp updatedAt) {
     this.id = id;
     this.name = name;
     this.createdAt = createdAt;
     this.updatedAt = updatedAt;
   }
 
-  protected Collection(String name) {
+  protected Collection(final String name) {
     this.name = name;
   }
   //#endregion Constructors
 
   //#region Getters and Setters
+
+  /** @return Name of table in database. */
   @Override
   public String tableName() {
     return TABLE_NAME;
   }
 
+  /** @return Primary key column name. */
   @Override
   public String primaryKeyName() {
     return PRIMARY_KEY_NAME;
   }
 
   @Override
-  public Integer getId() {
+  public final Integer getId() {
     return id;
   }
 
-  public void setId(Integer id) {
+  public final void setId(final Integer id) {
     this.id = id;
   }
 
-  public String getName() {
+  public final String getName() {
     return name;
   }
 
-  public void setName(String name) {
+  public final void setName(final String name) {
     this.name = name;
   }
 
-  public Timestamp getCreatedAt() {
+  public final Timestamp getCreatedAt() {
     return createdAt;
   }
 
-  public void setCreatedAt(Timestamp createdAt) {
+  public final void setCreatedAt(final Timestamp createdAt) {
     this.createdAt = createdAt;
   }
 
-  public Timestamp getUpdatedAt() {
+  public final Timestamp getUpdatedAt() {
     return updatedAt;
   }
 
-  public void setUpdatedAt(Timestamp updatedAt) {
+  public final void setUpdatedAt(final Timestamp updatedAt) {
     this.updatedAt = updatedAt;
   }
 
@@ -90,8 +102,15 @@ public abstract class Collection implements SavableDeletableEntity {
   //#endregion Getters and Setters
 
   //#region Database operations
+
+  /**
+   * Save this collection to database.
+   *
+   * @param connection {@link Connection} to use to save.
+   * @throws SQLException If an error occurs.
+   */
   @Override
-  public void save(Connection connection) throws SQLException {
+  public void save(final Connection connection) throws SQLException {
     if (this.id == null) {
       throw new NullPointerException("Collection#save can only be called on update");
     }
@@ -115,6 +134,8 @@ public abstract class Collection implements SavableDeletableEntity {
   //#endregion Database operations
 
   //#region String representations
+
+  /** @return String representation of this Collection. */
   @Override
   public String toString() {
     return defaultToString();
@@ -122,13 +143,21 @@ public abstract class Collection implements SavableDeletableEntity {
   //#endregion String representations
 
   //#region Equals
+
+  /** @return Hashcode of this instance. */
   @Override
   public int hashCode() {
     return id;
   }
 
+  /**
+   * Tests equality.
+   *
+   * @param o object to test.
+   * @return If the object is equal to this instance.
+   */
   @Override
-  public boolean equals(Object o) {
+  public boolean equals(final Object o) {
     if (o == null || o.getClass() != this.getClass()) {
       return false;
     }
