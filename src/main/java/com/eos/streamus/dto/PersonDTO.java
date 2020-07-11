9268552a517d915e9d2dd8afe362f73f46a12ab4
AkessonHenrik <henrik.akesson@heig-vd.ch>
2020-07-12 00:08:55 +0200

    Checkstyle: Fix PersonDTO

--- src/main/java/com/eos/streamus/dto/PersonDTO.java
+++ src/main/java/com/eos/streamus/dto/PersonDTO.java
@@ -3,29 +3,33 @@ package com.eos.streamus.dto;
 import javax.validation.constraints.NotNull;
 
 public class PersonDTO {
+  /** Id of person. */
   private Integer id;
 
+  /** First name of Person. */
   @NotNull
   private String firstName;
 
+  /** Last name of Person. */
   @NotNull
   private String lastName;
 
+  /** Date of birth of person as String (should be in format yyyy-MM-dd). */
   private String dateOfBirth;
 
-  public Integer getId() {
+  public final Integer getId() {
     return id;
   }
 
-  public String getFirstName() {
+  public final String getFirstName() {
     return firstName;
   }
 
-  public String getLastName() {
+  public final String getLastName() {
     return lastName;
   }
 
-  public String getDateOfBirth() {
+  public final String getDateOfBirth() {
     return dateOfBirth;
   }
 
