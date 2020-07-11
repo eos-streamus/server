4e3f8201c9b5708517fa4635aed2cbd15888cb47
AkessonHenrik <henrik.akesson@heig-vd.ch>
2020-07-11 23:50:54 +0200

    Checkstyle: Fix JsonSongPlaylistWriter

--- src/main/java/com/eos/streamus/writers/JsonSongPlaylistWriter.java
+++ src/main/java/com/eos/streamus/writers/JsonSongPlaylistWriter.java
@@ -4,7 +4,8 @@ import com.eos.streamus.models.SongPlaylist;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
-public class JsonSongPlaylistWriter extends JsonSongCollectionWriter {
+public final class JsonSongPlaylistWriter extends JsonSongCollectionWriter {
+  /** {@link com.eos.streamus.models.SongPlaylist} to write as Json. */
   private final SongPlaylist songPlaylist;
   public JsonSongPlaylistWriter(final SongPlaylist songPlaylist) {
     super(songPlaylist);
