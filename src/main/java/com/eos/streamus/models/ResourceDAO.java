d7a531476b246eafa7f5e0f9ee64552368cad062
AkessonHenrik <henrik.akesson@heig-vd.ch>
2020-07-11 22:48:05 +0200

    Checkstyle: Fix ResourceDAO

--- src/main/java/com/eos/streamus/models/ResourceDAO.java
+++ src/main/java/com/eos/streamus/models/ResourceDAO.java
@@ -5,10 +5,11 @@ import com.eos.streamus.exceptions.NoResultException;
 import java.sql.Connection;
 import java.sql.SQLException;
 
-public class ResourceDAO {
+public final class ResourceDAO {
   private ResourceDAO() {}
 
-  public static Resource findById(Integer id, Connection connection) throws SQLException, NoResultException {
+  public static Resource findById(final Integer id, final Connection connection)
+      throws SQLException, NoResultException {
     try {
       return VideoDAO.findById(id, connection);
     } catch (NoResultException e) {
