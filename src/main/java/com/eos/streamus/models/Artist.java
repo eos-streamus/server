6cfbb99925eb045aba599b28b94cf5c2cb4bdaf5
AkessonHenrik <henrik.akesson@heig-vd.ch>
2020-07-11 18:37:17 +0200

    Checkstyle: Fix Artist

--- src/main/java/com/eos/streamus/models/Artist.java
+++ src/main/java/com/eos/streamus/models/Artist.java
@@ -12,54 +12,65 @@ import java.util.logging.Logger;
 
 public abstract class Artist implements SavableDeletableEntity {
   //#region Static attributes
+  /** Artist table name in database. */
   public static final String TABLE_NAME = "Artist";
+  /** id column name in table. */
   public static final String PRIMARY_KEY_NAME = "id";
+  /** name column name in table. */
   public static final String NAME_COLUMN = "name";
+  /** AlbumArtist column name in table. */
   public static final String ALBUM_ARTIST_TABLE_NAME = "AlbumArtist";
+  /** AlbumArtist association table name. */
   public static final String ALBUM_ARTIST_ALBUM_ID = "idAlbum";
+  /** Artist id column name in AlbumArtist. */
   public static final String ALBUM_ARTIST_ARTIST_ID = "idArtist";
   //#endregion Static attributes
 
   //#region Instance attributes
+  /** Id of Artist. */
   private Integer id;
+  /** Name of the Artist. */
   private String name;
+  /** List of albums of Artist. */
   private final List<Album> albums = new ArrayList<>();
   //#endregion Instance attributes
 
   //#region Constructors
-  protected Artist(Integer id, String name) {
+  protected Artist(final Integer id, final String name) {
     this.id = id;
     this.name = name;
   }
 
-  protected Artist(String name) {
+  protected Artist(final String name) {
     this.name = name;
   }
   //#endregion Constructors
 
   //#region Getters and Setters
   @Override
-  public Integer getId() {
+  public final Integer getId() {
     return id;
   }
 
-  protected void setId(Integer id) {
+  protected final void setId(final Integer id) {
     this.id = id;
   }
 
-  public String getName() {
+  public final String getName() {
     return name;
   }
 
-  public void setName(String name) {
+  public final void setName(final String name) {
     this.name = name;
   }
 
+  /** @return Table name in database. */
   @Override
   public String tableName() {
     return TABLE_NAME;
   }
 
+  /** @return Primary key column name in database. */
   @Override
   public String primaryKeyName() {
     return PRIMARY_KEY_NAME;
@@ -71,8 +82,15 @@ public abstract class Artist implements SavableDeletableEntity {
   //#endregion Getters and Setters
 
   //#region Database operations
+
+  /**
+   * Save this Artist in database.
+   *
+   * @param connection {@link Connection} to use to save.
+   * @throws SQLException if an error occurs.
+   */
   @Override
-  public void save(Connection connection) throws SQLException {
+  public void save(final Connection connection) throws SQLException {
     if (this.getId() == null) {
       throw new NullPointerException("Artist#save can only be called on update");
     }
@@ -87,7 +105,7 @@ public abstract class Artist implements SavableDeletableEntity {
     }
   }
 
-  public void fetchAlbums(Connection connection) throws SQLException {
+  public final void fetchAlbums(final Connection connection) throws SQLException {
     try (PreparedStatement preparedStatement = connection.prepareStatement(
         String.format(
             "select * from %s where %s = ?", ALBUM_ARTIST_TABLE_NAME, ALBUM_ARTIST_ARTIST_ID
@@ -110,7 +128,7 @@ public abstract class Artist implements SavableDeletableEntity {
 
   //#region String representations
   @Override
-  public String toString() {
+  public final String toString() {
     return String.format(
         "%s[%s=%d]",
         getClass().getName(),
@@ -121,13 +139,20 @@ public abstract class Artist implements SavableDeletableEntity {
   //#endregion
 
   //#region Equals
+
+  /** @return hashcode of instance. */
   @Override
   public int hashCode() {
     return id;
   }
 
+  /**
+   * Checks if this instance is equal to given one.
+   * @param obj Object to test.
+   * @return If the instances are equal.
+   */
   @Override
-  public boolean equals(Object obj) {
+  public boolean equals(final Object obj) {
     if (obj == null) {
       return false;
     }
