a983df0ee4257572d8c29ff19cfcc6e13d570532
AkessonHenrik <henrik.akesson@heig-vd.ch>
2020-07-11 23:58:00 +0200

    Checkstyle: Fix StreamusApplicationConfiguration

--- src/main/java/com/eos/streamus/StreamusApplicationConfiguration.java
+++ src/main/java/com/eos/streamus/StreamusApplicationConfiguration.java
@@ -6,7 +6,7 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
-public class StreamusApplicationConfiguration {
+public final class StreamusApplicationConfiguration {
   @Bean
   public PasswordEncoder getPasswordEncoder() {
     return new BCryptPasswordEncoder();
