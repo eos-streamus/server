b46f13c4bdacd2a781dbd81f17e33ff579108810
AkessonHenrik <henrik.akesson@heig-vd.ch>
2020-07-11 23:49:00 +0200

    Checkstyle: Fix JsonSongCollectionListWriter

--- src/main/java/com/eos/streamus/writers/JsonSongCollectionListWriter.java
+++ src/main/java/com/eos/streamus/writers/JsonSongCollectionListWriter.java
@@ -7,12 +7,18 @@ import com.fasterxml.jackson.databind.node.ArrayNode;
 import java.util.List;
 
 public class JsonSongCollectionListWriter extends JsonArrayWriter {
+  /** List of {@link com.eos.streamus.models.SongCollection}s to write as Json. */
   private final List<? extends SongCollection> collections;
 
   public JsonSongCollectionListWriter(final List<? extends SongCollection> collections) {
     this.collections = collections;
   }
 
+  /**
+   * Writes the list of {@link SongCollection}s to json.
+   * @param arrayNode node to write collections to.
+   * @return Json node.
+   */
   @Override
   protected JsonNode getSpecificArrayJson(final ArrayNode arrayNode) {
     for (SongCollection collection : collections) {
