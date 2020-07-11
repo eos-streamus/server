4d58ba1251f27e325955da4a65e018971b403c07
AkessonHenrik <henrik.akesson@heig-vd.ch>
2020-07-11 23:32:20 +0200

    Checkstyle: FIx JwtService

--- src/main/java/com/eos/streamus/utils/JwtService.java
+++ src/main/java/com/eos/streamus/utils/JwtService.java
@@ -14,22 +14,27 @@ import java.time.temporal.ChronoUnit;
 import java.util.UUID;
 
 @Service
-public class JwtService {
+public final class JwtService {
+  /** Validity of a generated Json Web Token. */
+  @Value("${jwt.validityTimeInMinutes}")
+  private static final long JWT_VALIDITY_MINUTES = 5L;
+
+  /** Secret key for encoding JWTs. */
   @Value("${jwt.secret}")
   private String key;
 
-  public String createToken(User user) {
+  public String createToken(final User user) {
     return Jwts.builder()
                .signWith(Keys.hmacShaKeyFor(key.getBytes()))
                .claim("userId", user.getId())
                .claim("email", user.getEmail())
                .setId(UUID.randomUUID().toString())
                .setIssuedAt(Date.from(Instant.now()))
-               .setExpiration(Date.from(Instant.now().plus(5l, ChronoUnit.MINUTES)))
+               .setExpiration(Date.from(Instant.now().plus(JWT_VALIDITY_MINUTES, ChronoUnit.MINUTES)))
                .compact();
   }
 
-  public Jws<Claims> decode(String jwtToken) {
+  public Jws<Claims> decode(final String jwtToken) {
     return Jwts.parserBuilder()
         .setSigningKey(Keys.hmacShaKeyFor(key.getBytes()))
         .build()
