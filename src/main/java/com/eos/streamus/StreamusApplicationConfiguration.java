547e01caf3f16e8af8cd6a243baf1a246bf85325
AkessonHenrik <henrik.akesson@heig-vd.ch>
2020-08-01 21:24:58 +0200

    Allow cors from dev client

--- src/main/java/com/eos/streamus/StreamusApplicationConfiguration.java
+++ src/main/java/com/eos/streamus/StreamusApplicationConfiguration.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SuppressWarnings("CheckStyle")
 @Configuration
@@ -12,4 +14,15 @@ public class StreamusApplicationConfiguration {
   public PasswordEncoder getPasswordEncoder() {
     return new BCryptPasswordEncoder();
   }
+
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/*").allowedOrigins("http://localhost:4200");
+      }
+    };
+  }
+
 }
