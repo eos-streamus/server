4dcacfe75ac0e24b2c5d3d1a251d3dcd5382bf5a
AkessonHenrik <henrik.akesson@heig-vd.ch>
2020-09-15 00:12:27 +0200

    Create JsonResourceWriterFactory.java

---
+++ src/main/java/com/eos/streamus/writers/JsonResourceWriterFactory.java
@@ -0,0 +1,27 @@
+package com.eos.streamus.writers;
+
+import com.eos.streamus.models.Film;
+import com.eos.streamus.models.Resource;
+import com.eos.streamus.models.Series;
+import com.eos.streamus.models.Song;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+
+public class JsonResourceWriterFactory {
+  private JsonResourceWriterFactory() {}
+
+  public static JsonResourceWriter getWriterFor(Resource resource) {
+    if (resource instanceof Song) {
+      return new JsonSongWriter(resource);
+    } else if (resource instanceof Film) {
+      return new JsonFilmWriter((Film) resource);
+    } else if (resource instanceof Series.Episode) {
+      return new JsonEpisodeWriter((Series.Episode) resource);
+    }
+    return new JsonResourceWriter(resource) {
+      @Override
+      protected ObjectNode addSpecificJsonAttributes(final ObjectNode objectNode) {
+        return objectNode;
+      }
+    };
+  }
+}
