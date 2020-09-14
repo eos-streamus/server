c7bab00ca2f99aa2d942f3a868e5a1898561af3c
AkessonHenrik <henrik.akesson@heig-vd.ch>
2020-09-15 00:12:39 +0200

    Update ActivityController.java

--- src/main/java/com/eos/streamus/controllers/ActivityController.java
+++ src/main/java/com/eos/streamus/controllers/ActivityController.java
@@ -2,12 +2,16 @@ package com.eos.streamus.controllers;
 
 import com.eos.streamus.exceptions.NoResultException;
 import com.eos.streamus.models.Activity;
+import com.eos.streamus.models.Collection;
+import com.eos.streamus.models.CollectionActivity;
+import com.eos.streamus.models.CollectionDAO;
 import com.eos.streamus.models.Resource;
 import com.eos.streamus.models.ResourceActivity;
 import com.eos.streamus.models.ResourceDAO;
 import com.eos.streamus.models.User;
 import com.eos.streamus.utils.IDatabaseConnector;
 import com.eos.streamus.utils.JwtService;
+import com.eos.streamus.writers.JsonCollectionActivityWriter;
 import com.eos.streamus.writers.JsonResourceActivityWriter;
 import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -52,6 +56,31 @@ public final class ActivityController implements CommonResponses {
       }
       return ResponseEntity.ok(new JsonResourceActivityWriter(resourceActivity).getJson());
     } catch (SQLException | NoResultException sqlException) {
+      logException(sqlException);
+      return internalServerError();
+    }
+  }
+
+  @GetMapping("/collectionActivity/{collectionId}")
+  public ResponseEntity<JsonNode> getOrCreateCollectionActivity(@RequestHeader final HttpHeaders headers,
+                                                                @PathVariable("collectionId") final int collectionId) {
+    try (Connection connection = databaseConnector.getConnection()) {
+      final String token = Objects.requireNonNull(headers.getFirst("Authorization")).substring(7);
+      final User user = User.findById(this.jwtService.decode(token).getBody().get("userId", Integer.class), connection);
+      final Collection collection = CollectionDAO.findById(collectionId, connection);
+      CollectionActivity collectionActivity = CollectionActivity.findByUserAndCollectionIds(
+          user.getId(),
+          collection.getId(),
+          connection
+      );
+      if (collectionActivity == null) {
+        collectionActivity = new CollectionActivity(user, collection);
+        collectionActivity.save(connection);
+      }
+      return ResponseEntity.ok(new JsonCollectionActivityWriter(collectionActivity).getJson());
+    } catch (NoResultException noResultException) {
+      return notFound();
+    } catch (SQLException sqlException) {
       logException(sqlException);
       return internalServerError();
     }
