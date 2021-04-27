0ea77820c99d4f4068569890891614678973c125
AkessonHenrik <henrik.akesson@outlook.com>
2021-04-28 01:38:32 +0200

    Update ResourcePathResolver.java

--- src/main/java/com/eos/streamus/utils/ResourcePathResolver.java
+++ src/main/java/com/eos/streamus/utils/ResourcePathResolver.java
@@ -9,14 +9,23 @@ import java.io.File;
 @Service
 @Scope(value = "singleton")
 public class ResourcePathResolver implements IResourcePathResolver {
+  /**
+   * Resource storage path.
+   */
   @Value("${resourcePath}")
   private String resourcePath;
 
+  /**
+   * {@inheritDoc}
+   */
   @Override
   public String getVideoDir() {
     return String.format("%s%s%s%s", resourcePath, File.separator, "video", File.separator);
   }
 
+  /**
+   * {@inheritDoc}
+   */
   @Override
   public String getAudioDir() {
     return String.format("%s%s%s%s", resourcePath, File.separator, "audio", File.separator);
