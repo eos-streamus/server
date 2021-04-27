fa40159b38c3f42d9f6a1cef5b4e28c05f52431e
AkessonHenrik <henrik.akesson@outlook.com>
2021-04-28 01:33:22 +0200

    Update IResourcePathResolver.java

--- src/main/java/com/eos/streamus/utils/IResourcePathResolver.java
+++ src/main/java/com/eos/streamus/utils/IResourcePathResolver.java
@@ -2,7 +2,13 @@ package com.eos.streamus.utils;
 
 public interface IResourcePathResolver {
 
+  /**
+   * @return Video storage directory path.
+   */
   String getVideoDir();
 
+  /**
+   * @return Audio storage directory path.
+   */
   String getAudioDir();
 }
