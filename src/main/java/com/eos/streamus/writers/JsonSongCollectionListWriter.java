e72c510539fc95b61c0d4b2e8b66844f70ba8edf
AkessonHenrik <henrik.akesson@outlook.com>
2021-04-28 01:26:58 +0200

    Update JsonSongCollectionListWriter.java

--- src/main/java/com/eos/streamus/writers/JsonSongCollectionListWriter.java
+++ src/main/java/com/eos/streamus/writers/JsonSongCollectionListWriter.java
@@ -7,6 +7,9 @@ import com.fasterxml.jackson.databind.node.ArrayNode;
 import java.util.List;
 
 public class JsonSongCollectionListWriter extends JsonArrayWriter {
+  /**
+   * {@link SongCollection} subclass instance to write.
+   */
   private final List<? extends SongCollection> collections;
 
   public JsonSongCollectionListWriter(final List<? extends SongCollection> collections) {
