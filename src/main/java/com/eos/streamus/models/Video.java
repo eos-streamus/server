966a3df4f67c699ade0f6fca8fad1919faf0f13e
AkessonHenrik <henrik.akesson@outlook.com>
2021-04-27 23:06:03 +0200

    Fix video except for constructors

--- src/main/java/com/eos/streamus/models/Video.java
+++ src/main/java/com/eos/streamus/models/Video.java
@@ -4,26 +4,40 @@ import java.sql.Timestamp;
 
 public abstract class Video extends Resource {
   //#region Static attributes
+  /**
+   * Table name in database.
+   */
   private static final String TABLE_NAME = "Video";
+  /**
+   * Primary key column name in database.
+   */
   private static final String PRIMARY_KEY_NAME = "idResource";
   //#endregion Static attributes
 
   //#region Constructors
-  protected Video(Integer id, String path, String name, Timestamp createdAt, Integer duration) {
+  protected Video(final Integer id, final String path, final String name,
+                  final Timestamp createdAt, final Integer duration) {
     super(id, path, name, createdAt, duration);
   }
 
-  protected Video(String path, String name, Integer duration) {
+  protected Video(final String path, final String name, final Integer duration) {
     super(path, name, duration);
   }
   //#endregion Constructors
 
   //#region Getters and Setters
+
+  /**
+   * {@inheritDoc}
+   */
   @Override
   public String tableName() {
     return TABLE_NAME;
   }
 
+  /**
+   * {@inheritDoc}
+   */
   @Override
   public String primaryKeyName() {
     return PRIMARY_KEY_NAME;
