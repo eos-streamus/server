a2679d08faa8be97136ad56218af92eefedd0b88
AkessonHenrik <henrik.akesson@outlook.com>
2021-04-28 01:32:46 +0200

    Update FileInfo.java

--- src/main/java/com/eos/streamus/utils/FileInfo.java
+++ src/main/java/com/eos/streamus/utils/FileInfo.java
@@ -7,11 +7,20 @@ import com.fasterxml.jackson.databind.node.ObjectNode;
  * Class that contains FFProbe information about a file. For now, used only for duration.
  */
 public class FileInfo {
+  /**
+   * Duration.
+   */
   private final int duration;
+  /**
+   * Has video track.
+   */
   private boolean isVideo;
+  /**
+   * Has audio track.
+   */
   private boolean isAudio;
 
-  FileInfo(ObjectNode jsonData) {
+  FileInfo(final ObjectNode jsonData) {
     this.duration = jsonData.get("format").get("duration").asInt();
     for (final JsonNode stream : jsonData.get("streams")) {
       if (stream.get("codec_type").asText().equals("audio")) {
@@ -22,14 +31,30 @@ public class FileInfo {
     }
   }
 
+  /**
+   * @return File duration.
+   */
   public final int getDuration() {
     return duration;
   }
 
-  public final boolean isAudio() {return isAudio;}
+  /**
+   * @return If file has audio content.
+   */
+  public final boolean isAudio() {
+    return isAudio;
+  }
 
-  public final boolean isVideo() {return isVideo;}
+  /**
+   * @return If file is a video.
+   */
+  public final boolean isVideo() {
+    return isVideo;
+  }
 
+  /**
+   * @return If file is exclusively audio.
+   */
   public final boolean isAudioOnly() {
     return isAudio && !isVideo;
   }
