72ea9a6df7d34b7d5a1d9688206c5d87f4406b5b
AkessonHenrik <henrik.akesson@outlook.com>
2021-04-28 01:37:57 +0200

    Update Pair.java

--- src/main/java/com/eos/streamus/utils/Pair.java
+++ src/main/java/com/eos/streamus/utils/Pair.java
@@ -1,36 +1,74 @@
 package com.eos.streamus.utils;
 
+import java.util.Objects;
+
 public class Pair<K, V> {
+  /**
+   * Key of Pair.
+   */
   private K key;
+  /**
+   * Value of Pair.
+   */
   private V value;
 
-  public Pair(K key, V value) {
+  public Pair(final K key, final V value) {
     this.key = key;
     this.value = value;
   }
 
+  /**
+   * @return Key of Pair.
+   */
   public K getKey() {
     return key;
   }
 
+  /**
+   * @return Value of Pair.
+   */
   public V getValue() {
     return value;
   }
 
-  public void setKey(K key) {
+  /**
+   * Set key of Pair.
+   *
+   * @param key Key to set.
+   */
+  public void setKey(final K key) {
     this.key = key;
   }
 
-  public void setValue(V value) {
+  /**
+   * Set Value of Pair.
+   *
+   * @param value Value to set.
+   */
+  public void setValue(final V value) {
     this.value = value;
   }
 
+  /**
+   * @return HashCode combined from key hashCode and value hashCode.
+   */
   @Override
   public int hashCode() {
-    return key.hashCode() * 31 + value.hashCode();
+    return Objects.hash(key.hashCode(), value.hashCode());
   }
 
-  public boolean equals(Object obj) {
+  /**
+   * Returns whether the given Object is equal.
+   * Equal if:
+   * - Not null
+   * - Same class
+   * - Same value (both null or equal)
+   * - Same key (both null or equal)
+   *
+   * @param obj Object to compare.
+   * @return True if all conditions are met.
+   */
+  public boolean equals(final Object obj) {
     if (obj == null || obj.getClass() != getClass()) {
       return false;
     }
@@ -44,6 +82,9 @@ public class Pair<K, V> {
     return key.equals(pair.key) && value.equals(pair.value);
   }
 
+  /**
+   * {@inheritDoc}
+   */
   public String toString() {
     return String.format("<%s, %s>", key, value);
   }
