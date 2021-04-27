8ed4e9385150d9b281a13f2825e5b014d1de6fd0
AkessonHenrik <henrik.akesson@outlook.com>
2021-04-28 01:30:58 +0200

    Update IDatabaseConnector.java

--- src/main/java/com/eos/streamus/utils/IDatabaseConnector.java
+++ src/main/java/com/eos/streamus/utils/IDatabaseConnector.java
@@ -4,5 +4,11 @@ import java.sql.Connection;
 import java.sql.SQLException;
 
 public interface IDatabaseConnector {
+  /**
+   * Get {@link Connection} to database.
+   *
+   * @return Connection.
+   * @throws SQLException if an error occurred.
+   */
   Connection getConnection() throws SQLException;
 }
