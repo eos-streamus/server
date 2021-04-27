64f18867eb49d465a27085ddf5f2f097d58372d5
AkessonHenrik <henrik.akesson@outlook.com>
2021-04-28 01:12:11 +0200

    Fix JsonObjectWriter

--- src/main/java/com/eos/streamus/writers/JsonObjectWriter.java
+++ src/main/java/com/eos/streamus/writers/JsonObjectWriter.java
@@ -4,11 +4,14 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
 abstract class JsonObjectWriter implements JsonWriter {
+  /**
+   * {@inheritDoc}
+   */
   @Override
   public final JsonNode getJson() {
     final ObjectNode objectNode = new ObjectNode(new JsonWriterNodeFactory());
     return getSpecificJson(objectNode);
   }
 
-  protected abstract JsonNode getSpecificJson(final ObjectNode objectNode);
+  protected abstract JsonNode getSpecificJson(ObjectNode objectNode);
 }
