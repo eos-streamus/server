b1add15189d67590ebb04c2d8e1b31b9446d1c66
AkessonHenrik <henrik.akesson@outlook.com>
2021-04-28 01:27:28 +0200

    Update JsonSongPlaylistWriter.java

--- src/main/java/com/eos/streamus/writers/JsonSongPlaylistWriter.java
+++ src/main/java/com/eos/streamus/writers/JsonSongPlaylistWriter.java
@@ -5,7 +5,11 @@ import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
 public class JsonSongPlaylistWriter extends JsonSongCollectionWriter {
+  /**
+   * {@link SongPlaylist} to write.
+   */
   private final SongPlaylist songPlaylist;
+
   public JsonSongPlaylistWriter(final SongPlaylist songPlaylist) {
     super(songPlaylist);
     this.songPlaylist = songPlaylist;
