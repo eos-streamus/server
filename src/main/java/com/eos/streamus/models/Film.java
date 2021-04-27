431ef484ab78d1fbf7d14554895a621f4c472cab
AkessonHenrik <henrik.akesson@outlook.com>
2021-04-27 23:09:05 +0200

    Fix Film except for constructor

--- src/main/java/com/eos/streamus/models/Film.java
+++ src/main/java/com/eos/streamus/models/Film.java
@@ -8,33 +8,56 @@ import java.util.List;
 
 public class Film extends Video {
   //#region Static attributes
+  /**
+   * Table name in the database.
+   */
   private static final String TABLE_NAME = "Film";
-  private static final String VIEW_NAME = "vfilm";
+  /**
+   * View name in the database.
+   */
+  private static final String VIEW_NAME = "vFilm";
+  /**
+   * Creation function name in the database.
+   */
   private static final String CREATION_FUNCTION_NAME = "createFilm";
+  /**
+   * Primary key name in the database.
+   */
   private static final String PRIMARY_KEY_NAME = "idVideo";
   //#endregion Static attributes
 
   //#region Constructors
-  private Film(Integer id, String path, String name, Timestamp createdAt, Integer duration) {
+  private Film(final Integer id, final String path, final String name,
+               final Timestamp createdAt, final Integer duration) {
     super(id, path, name, createdAt, duration);
   }
 
-  public Film(String path, String name, Integer duration) {
+  public Film(final String path, final String name, final Integer duration) {
     super(path, name, duration);
   }
   //#endregion Constructors
 
   //#region Getters and Setters
+
+  /**
+   * {@inheritDoc}
+   */
   @Override
   public String creationFunctionName() {
     return CREATION_FUNCTION_NAME;
   }
 
+  /**
+   * {@inheritDoc}
+   */
   @Override
   public String tableName() {
     return TABLE_NAME;
   }
 
+  /**
+   * {@inheritDoc}
+   */
   @Override
   public String primaryKeyName() {
     return PRIMARY_KEY_NAME;
@@ -42,7 +65,17 @@ public class Film extends Video {
   //#endregion Getters and Setters
 
   //#region Database operations
-  public static Film findById(int id, Connection connection) throws SQLException, NoResultException {
+
+  /**
+   * Finds a Film by given id.
+   *
+   * @param id Id of Film to find.
+   * @param connection {@link Connection} to use to perform the operation.
+   * @throws SQLException If an error occurred while performing the database operation.
+   * @throws NoResultException If no film by given id was found.
+   * @return Found Film.
+   */
+  public static Film findById(final int id, final Connection connection) throws SQLException, NoResultException {
     try (PreparedStatement statement = connection.prepareStatement(
         String.format(
             "select * from %s where %s = ?",
@@ -66,7 +99,14 @@ public class Film extends Video {
     }
   }
 
-  public static List<Film> all(Connection connection) throws SQLException {
+  /**
+   * Fetches all Films from database.
+   *
+   * @param connection {@link Connection} to use to perform the operation.
+   * @throws SQLException If an error occurred while performing the database operation.
+   * @return List of all Films.
+   */
+  public static List<Film> all(final Connection connection) throws SQLException {
     List<Film> allFilms = new ArrayList<>();
     try (PreparedStatement preparedStatement = connection.prepareStatement(
         String.format("select * from %s", VIEW_NAME)
