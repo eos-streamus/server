f9a36cb55580cd6cd2ee852ea70958cb88644a57
AkessonHenrik <henrik.akesson@outlook.com>
2021-04-28 01:25:27 +0200

    Update JsonPersonWriter.java

--- src/main/java/com/eos/streamus/writers/JsonPersonWriter.java
+++ src/main/java/com/eos/streamus/writers/JsonPersonWriter.java
@@ -8,6 +8,9 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 
 public class JsonPersonWriter extends JsonObjectWriter {
+  /**
+   * {@link Person} to write.
+   */
   private final Person person;
 
   public JsonPersonWriter(final Person person) {
@@ -21,8 +24,8 @@ public class JsonPersonWriter extends JsonObjectWriter {
   protected JsonNode getSpecificJson(final ObjectNode objectNode) {
     DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
     objectNode.put("id", person.getId())
-              .put("firstName", person.getFirstName())
-              .put("lastName", person.getLastName());
+        .put("firstName", person.getFirstName())
+        .put("lastName", person.getLastName());
     objectNode.put("dateOfBirth", person.getDateOfBirth() == null ? null : person.getDateOfBirth().getTime());
     objectNode.put("createdAt", dateFormat.format(person.getCreatedAt()));
     objectNode.put("updatedAt", dateFormat.format(person.getUpdatedAt()));
