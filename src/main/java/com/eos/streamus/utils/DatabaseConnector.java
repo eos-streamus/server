3f16bd0f0b613dec0bc705df6924e656c319cc0b
AkessonHenrik <henrik.akesson@outlook.com>
2021-04-28 01:31:13 +0200

    Update DatabaseConnector.java

--- src/main/java/com/eos/streamus/utils/DatabaseConnector.java
+++ src/main/java/com/eos/streamus/utils/DatabaseConnector.java
@@ -12,24 +12,45 @@ import org.springframework.stereotype.Service;
 @Service
 @Scope(value = "singleton")
 public class DatabaseConnector implements IDatabaseConnector {
+  /**
+   * Jdbc url.
+   */
   @Value("${jdbc.url}")
   private String url;
 
+  /**
+   * Jdbc host.
+   */
   @Value("${jdbc.host}")
   private String host;
 
+  /**
+   * Jdbc port.
+   */
   @Value("${jdbc.port}")
   private int port;
 
+  /**
+   * Jdbc databaseName.
+   */
   @Value("${jdbc.databaseName}")
   private String databaseName;
 
+  /**
+   * Database user.
+   */
   @Value("${database.user}")
   private String user;
 
+  /**
+   * Database password.
+   */
   @Value("${database.password}")
   private String password;
 
+  /**
+   * {@inheritDoc}
+   */
   @Override
   public Connection getConnection() throws SQLException {
     return DriverManager.getConnection(String.format("%s%s:%d/%s", url, host, port, databaseName), user, password);
