b39cd0bc21d492851a123d94933332b2552810b6
AkessonHenrik <henrik.akesson@outlook.com>
2021-04-27 23:17:46 +0200

    Fix VideoCollection

--- src/main/java/com/eos/streamus/models/VideoCollection.java
+++ src/main/java/com/eos/streamus/models/VideoCollection.java
@@ -4,26 +4,39 @@ import java.sql.Timestamp;
 
 public abstract class VideoCollection extends Collection {
   //#region Static attributes
+  /**
+   * Table name in database.
+   */
   protected static final String TABLE_NAME = "VideoCollection";
+  /**
+   * Primary key column name in database.
+   */
   protected static final String PRIMARY_KEY_NAME = "idCollection";
   //#endregion Static attributes
 
   //#region Constructors
-  protected VideoCollection(Integer id, String name, Timestamp createdAt, Timestamp updatedAt) {
+  protected VideoCollection(final Integer id, final String name, final Timestamp createdAt, final Timestamp updatedAt) {
     super(id, name, createdAt, updatedAt);
   }
 
-  protected VideoCollection(String name) {
+  protected VideoCollection(final String name) {
     super(name);
   }
   //#endregion Constructors
 
   //#region Getters and Setters
+
+  /**
+   * {@inheritDoc}
+   */
   @Override
   public String tableName() {
     return TABLE_NAME;
   }
 
+  /**
+   * {@inheritDoc}
+   */
   @Override
   public String primaryKeyName() {
     return PRIMARY_KEY_NAME;
