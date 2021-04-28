29ec86bf99c53babe52dcae5bac2995ce9d97246
AkessonHenrik <henrik.akesson@outlook.com>
2021-04-28 17:52:05 +0200

    Update Builder.java

--- src/main/java/com/eos/streamus/models/Builder.java
+++ src/main/java/com/eos/streamus/models/Builder.java
@@ -1,8 +1,6 @@
 package com.eos.streamus.models;
 
 public interface Builder<T> {
-  /**
-   * @return An instance of T based off given values.
-   */
+  /** @return An instance of T based off given values. */
   T build();
 }
