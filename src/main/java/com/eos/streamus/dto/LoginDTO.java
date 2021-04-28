db0305914575650807b96e4dde81882e0f4369b9
AkessonHenrik <henrik.akesson@outlook.com>
2021-04-28 10:31:05 +0200

    Update LoginDTO.java

--- src/main/java/com/eos/streamus/dto/LoginDTO.java
+++ src/main/java/com/eos/streamus/dto/LoginDTO.java
@@ -3,15 +3,27 @@ package com.eos.streamus.dto;
 import javax.validation.constraints.NotNull;
 
 public class LoginDTO {
+  /**
+   * Login email.
+   */
   @NotNull
   private String email;
+  /**
+   * Login password.
+   */
   @NotNull
   private String password;
 
+  /**
+   * @return Email of login data.
+   */
   public String getEmail() {
     return email;
   }
 
+  /**
+   * @return Password of login data.
+   */
   public String getPassword() {
     return password;
   }
