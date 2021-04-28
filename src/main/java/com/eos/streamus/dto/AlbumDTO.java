3b9e1e3f5b38e512ddc1260f10a3c73397e7af7f
AkessonHenrik <henrik.akesson@outlook.com>
2021-04-28 10:28:02 +0200

    Update AlbumDTO.java

--- src/main/java/com/eos/streamus/dto/AlbumDTO.java
+++ src/main/java/com/eos/streamus/dto/AlbumDTO.java
@@ -5,15 +5,27 @@ import java.util.Date;
 import java.util.List;
 
 public class AlbumDTO extends SongCollectionDTO {
+  /**
+   * Ids of contributing {@link com.eos.streamus.models.Artist}s.
+   */
   @NotNull
   private List<Integer> artistIds;
+  /**
+   * Release date of {@link com.eos.streamus.models.Album}.
+   */
   @NotNull
   private Date releaseDate;
 
+  /**
+   * @return List of Artist ids of the Album.
+   */
   public List<Integer> getArtistIds() {
     return artistIds;
   }
 
+  /**
+   * @return Release date of Album.
+   */
   public Date getReleaseDate() {
     return releaseDate;
   }
