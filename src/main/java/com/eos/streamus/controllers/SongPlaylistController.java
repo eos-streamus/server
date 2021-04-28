c030feb7815a41b4add8befe6fe947ace50d92e3
AkessonHenrik <henrik.akesson@outlook.com>
2021-04-28 11:26:05 +0200

    Update SongPlaylistController.java

--- src/main/java/com/eos/streamus/controllers/SongPlaylistController.java
+++ src/main/java/com/eos/streamus/controllers/SongPlaylistController.java
@@ -29,55 +29,105 @@ import java.sql.SQLException;
 @RestController
 public class SongPlaylistController extends SongCollectionController {
 
+  /**
+   * {@link SongPlaylistDTOValidator} to use.
+   */
   @Autowired
   private SongPlaylistDTOValidator songPlaylistDTOValidator;
 
+  /**
+   * Get a SongPlaylist by id.
+   *
+   * @param id Id of SongPlaylist to get.
+   * @return Song playlist in JSON format.
+   */
   @GetMapping("/songplaylist/{id}")
   public ResponseEntity<JsonNode> getSongPlaylist(@PathVariable final int id) {
     return getSongCollectionById(id);
   }
 
+  /**
+   * Create a SongPlaylist.
+   *
+   * @param songPlaylistDTO SongPlaylist data.
+   * @param result          BindingResult to add validation results to.
+   * @return Created SongPlaylist data in JSON format.
+   */
   @PostMapping("/songplaylist")
-  public ResponseEntity<JsonNode> createSongPlaylist(
-      @Valid @RequestBody final SongPlaylistDTO songPlaylistDTO,
-      BindingResult result
-  ) {
+  public ResponseEntity<JsonNode> createSongPlaylist(@Valid @RequestBody final SongPlaylistDTO songPlaylistDTO,
+                                                     final BindingResult result) {
     return createSongCollection(songPlaylistDTO, result);
   }
 
+  /**
+   * Delete a SongPlaylist by id.
+   *
+   * @param id Id of SongPlaylist to delete.
+   * @return Confirmation message.
+   */
   @DeleteMapping("/songplaylist/{id}")
   public ResponseEntity<String> deleteSongPlaylist(@PathVariable final int id) {
     return deleteSongCollection(id);
   }
 
+  /**
+   * Delete a song from a SongCollection.
+   *
+   * @param songCollectionId Id of SongCollection.
+   * @param songId           Id of Song.
+   * @return Updated SongCollection data in JSON.
+   */
   @DeleteMapping("/songplaylist/{songCollectionId}/{songId}")
   public ResponseEntity<JsonNode> deleteSongFromSongPlaylist(@PathVariable final int songCollectionId,
                                                              @PathVariable final int songId) {
     return deleteSongFromSongCollection(songCollectionId, songId);
   }
 
-  @PutMapping("/songplaylist/{id}/tracks")
-  public ResponseEntity<JsonNode> addOrMoveTrackInSongPlaylist(@PathVariable final int id,
-                                                                 @Valid @RequestBody final TrackDTO trackData) {
-    return addOrMoveTrackInSongCollection(id, trackData);
+  /**
+   * Add or move a Track in a SongPlaylist.
+   *
+   * @param songPlaylistId Id
+   * @param trackData      Track data.
+   * @return Updated SongPlaylist data in JSON.
+   */
+  @PutMapping("/songplaylist/{songPlaylistId}/tracks")
+  public ResponseEntity<JsonNode> addOrMoveTrackInSongPlaylist(@PathVariable final int songPlaylistId,
+                                                               @Valid @RequestBody final TrackDTO trackData) {
+    return addOrMoveTrackInSongCollection(songPlaylistId, trackData);
   }
 
+  /**
+   * Add a Song to a SongPlaylist.
+   *
+   * @param songPlaylistId SongPlaylist id.
+   * @param songId         Song id.
+   * @return Updated SongPlaylist in JSON format.
+   */
   @PostMapping("/songplaylist/{songPlaylistId}/{songId}")
   public ResponseEntity<JsonNode> addSongToSongPlaylist(@PathVariable final int songPlaylistId,
                                                         @PathVariable final int songId) {
     return addSongToCollection(songPlaylistId, songId);
   }
 
+  /**
+   * {@inheritDoc}
+   */
   @Override
   protected SongCollectionDTOValidator getSongCollectionDTOValidator() {
     return songPlaylistDTOValidator;
   }
 
+  /**
+   * {@inheritDoc}
+   */
   @Override
   protected JsonSongCollectionWriter jsonSongCollectionWriter(final SongCollection songCollection) {
     return new JsonSongPlaylistWriter((SongPlaylist) songCollection);
   }
 
+  /**
+   * {@inheritDoc}
+   */
   @Override
   protected SongCollection createSpecificCollection(
       final com.eos.streamus.dto.SongCollectionDTO songCollectionDTO,
