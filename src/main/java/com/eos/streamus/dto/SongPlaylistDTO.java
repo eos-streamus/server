7b9d98567832bdf51edb070717ad34ac2f09efcf
AkessonHenrik <henrik.akesson@outlook.com>
2021-04-28 17:39:07 +0200

    Update SongPlaylistDTO.java

--- src/main/java/com/eos/streamus/dto/SongPlaylistDTO.java
+++ src/main/java/com/eos/streamus/dto/SongPlaylistDTO.java
@@ -3,15 +3,11 @@ package com.eos.streamus.dto;
 import javax.validation.constraints.NotNull;
 
 public class SongPlaylistDTO extends SongCollectionDTO {
-  /**
-   * Owner User id.
-   */
+  /** Owner User id. */
   @NotNull
   private Integer userId;
 
-  /**
-   * @return Owner User id.
-   */
+  /** @return Owner User id. */
   public Integer getUserId() {
     return userId;
   }
