4e867bbf5356afd13fe466823f361f5dce3b8a8f
AkessonHenrik <henrik.akesson@outlook.com>
2021-04-28 10:28:53 +0200

    Update BandDTO.java

--- src/main/java/com/eos/streamus/dto/BandDTO.java
+++ src/main/java/com/eos/streamus/dto/BandDTO.java
@@ -4,10 +4,16 @@ import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 public class BandDTO {
+  /**
+   * Name of the Band.
+   */
   @NotNull
   @Size(min = 1)
   private String name;
 
+  /**
+   * @return Name of the Band
+   */
   public String getName() {
     return name;
   }
