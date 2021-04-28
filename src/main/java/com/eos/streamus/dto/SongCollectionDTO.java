5efd87f62453789932b601852271868cf0cd7286
AkessonHenrik <henrik.akesson@outlook.com>
2021-04-28 10:33:58 +0200

    Update SongCollectionDTO.java

--- src/main/java/com/eos/streamus/dto/SongCollectionDTO.java
+++ src/main/java/com/eos/streamus/dto/SongCollectionDTO.java
@@ -5,16 +5,28 @@ import javax.validation.constraints.Size;
 import java.util.List;
 
 public abstract class SongCollectionDTO {
+  /**
+   * Name of the SongCollection.
+   */
   @NotNull
   @Size(min = 1)
   private String name;
+  /**
+   * {@link TrackDTO}s of the SongCollection.
+   */
   @NotNull
   private List<TrackDTO> tracks;
 
+  /**
+   * @return Name of SongCollection.
+   */
   public String getName() {
     return name;
   }
 
+  /**
+   * @return Tracks of SongCollection.
+   */
   public List<TrackDTO> getTracks() {
     return tracks;
   }
