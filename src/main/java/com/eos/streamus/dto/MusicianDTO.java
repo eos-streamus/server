71bf98723dae3068d918f8de7a90f4df0a2aa8fd
AkessonHenrik <henrik.akesson@outlook.com>
2021-04-28 17:38:40 +0200

    Update MusicianDTO.java

--- src/main/java/com/eos/streamus/dto/MusicianDTO.java
+++ src/main/java/com/eos/streamus/dto/MusicianDTO.java
@@ -1,36 +1,24 @@
 package com.eos.streamus.dto;
 
 public class MusicianDTO {
-  /**
-   * Id of Musician.
-   */
+  /** Id of Musician. */
   private Integer id;
-  /**
-   * Name of Musician.
-   */
+  /** Name of Musician. */
   private String name;
-  /**
-   * Person data of Musician.
-   */
+  /** Person data of Musician. */
   private PersonDTO person;
 
-  /**
-   * @return Id of Musician.
-   */
+  /** @return Id of Musician. */
   public Integer getId() {
     return id;
   }
 
-  /**
-   * @return Name of Musician.
-   */
+  /** @return Name of Musician. */
   public String getName() {
     return name;
   }
 
-  /**
-   * @return {@link PersonDTO} of Musician.
-   */
+  /** @return {@link PersonDTO} of Musician. */
   public PersonDTO getPerson() {
     return person;
   }
