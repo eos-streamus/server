d34568496826a44659415078ca45ab6c7f6d22df
AkessonHenrik <henrik.akesson@outlook.com>
2021-04-28 10:35:06 +0200

    Update TrackDTO.java

--- src/main/java/com/eos/streamus/dto/TrackDTO.java
+++ src/main/java/com/eos/streamus/dto/TrackDTO.java
@@ -1,13 +1,25 @@
 package com.eos.streamus.dto;
 
 public class TrackDTO {
+  /**
+   * Track number.
+   */
   private int trackNumber;
+  /**
+   * {@link com.eos.streamus.models.Song} id.
+   */
   private int songId;
 
+  /**
+   * @return Track number.
+   */
   public int getTrackNumber() {
     return trackNumber;
   }
 
+  /**
+   * @return {@link com.eos.streamus.models.Song} id.
+   */
   public int getSongId() {
     return songId;
   }
